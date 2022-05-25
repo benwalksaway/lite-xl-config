062833066c65998c9a1ef9ebda8da27eb968838a
Ben Larisch <benlarisch2019@gmail.com>
2022-05-25 12:04:17 +0200

    all: add

---
+++ colors/gruvbox_ish.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#282828" }
+style.background2 = { common.color "#1d2021" }
+style.background3 = { common.color "#1d2021" }
+style.text = { common.color "#928374" }
+style.caret = { common.color "#fbf1c7" }
+style.accent = { common.color "#ebdbb2" }
+style.dim = { common.color "#928374" }
+style.divider = { common.color "#665c54" }
+style.selection = { common.color "#3c3836" }
+style.line_number = { common.color "#928374" }
+style.line_number2 = { common.color "#ebdbb2" }
+style.line_highlight = { common.color "#32302f" }
+style.scrollbar = { common.color "#928374" }
+style.scrollbar2 = { common.color "#fbf1c7" }
+
+style.syntax["normal"] = { common.color "###fcf0bd" }
+style.syntax["symbol"] = { common.color "###fcf0bd" }
+style.syntax["comment"] = { common.color "#928374" }
+style.syntax["keyword"] = { common.color "#d75f5f" }
+style.syntax["keyword2"] = { common.color "#d69617" }
+style.syntax["number"] = { common.color "#d3869b" }
+style.syntax["literal"] = { common.color "#d3869b" }
+style.syntax["string"] = { common.color "#8b9553" }
+style.syntax["operator"] = { common.color "#ebdbb2" }
+style.syntax["function"] = { common.color "#a8a81c" }
+
